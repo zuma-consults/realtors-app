c31c3b94e1b8fe023fb2a33805253074a71cc0ea
chisom chima <chimachisom360@gmail.com>
2024-02-20 19:53:12 +0100

    ui tweeks

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,16 +3,16 @@ import DoneIcon from "@mui/icons-material/Done";
 
 function AboutSection() {
   return (
-    <div className="w-full h-[100vh] md:h-[90vh] grid md:grid-cols-2 md:p-10 p-5 text-gray-500 font-normal">
+    <div className="w-full h-[100vh] md:h-[90vh] grid md:grid-cols-2 md:p-10 p-5 gap-3 text-gray-500 font-normal">
       <div className="h-full w-full relative" data-aos="fade-right">
         <div className="image h-full w-full bg-ra-darkgreen"></div>
         <img
           src="/house1.jpg"
           alt=""
-          className="absolute md:top-5 md:left-10 top-5 left-5 h-[90%] md:h-[85%] w-[90%]"
+          className="absolute md:top-10 md:left-10 top-5 left-5 h-[90%] md:h-[85%] w-[90%] md:w-full"
         />
       </div>
-      <div className="flex flex-col items-start md:justify-center mt-10 md:mt-0 gap-4 md:p-10 w-[80%]" data-aos="fade-left">
+      <div className=" h-full flex flex-col items-start md:justify-center mt-10 md:mt-0 gap-5 md:pl-20 w-[80%] " data-aos="fade-left">
         <h1 className="font-bold text-3xl text-ra-text">
           #1 Place To Find The Perfect Property
         </h1>
